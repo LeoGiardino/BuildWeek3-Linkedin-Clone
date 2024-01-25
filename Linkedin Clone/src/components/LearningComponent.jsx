9aeb18354a7d9cc7932a7bbd3f17f7027b96616f
Victor <victor246257@gmail.com>
2024-01-25 15:47:20 +0100

    modifica

--- Linkedin Clone/src/components/LearningComponent.jsx
+++ Linkedin Clone/src/components/LearningComponent.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import comunicazione from "../assets/comunicazione.jpg"
 export default function LearningComponent() {
   return (
     <>
-        <div className='d-flex'>
-            <p className='p-2'>
-                
+        <div className='d-flex text-white border-top border-bottom border-black pt-3'>
+            <p className='w-75'>
+                <img className='w-100 h-100' src={comunicazione} alt="comunicazione" />
            </p>
-           <p>
-                ciao
+           <p className='ms-2 visualizzazioni'>
+                <b>Comunicazione ad alto rischio</b> <br />
+                <span className='text-secondary'><b>2.368 visualizzazioni </b></span>
            </p>
         </div>
     </>
