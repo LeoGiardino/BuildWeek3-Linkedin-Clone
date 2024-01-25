db1ec542e7ab8c4aded6df88169e6f031e27ae79
Andrea Boschetto <andreaboschetto@MacBook-Pro-di-Andrea.local>
2024-01-25 18:07:53 +0100

    componente lingua

--- Linkedin Clone/src/components/RightProfile.jsx
+++ Linkedin Clone/src/components/RightProfile.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/RightProfile.css'
 import LearningComponent from './LearningComponent'
+import LanguageComponent from './LanguageComponent'
 
 export default function RightProfile() {
   return (
     <div className='destra'>
+        <LanguageComponent />
         <LearningComponent/>
     </div>
   )
