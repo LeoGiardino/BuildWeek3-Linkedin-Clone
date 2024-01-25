85277e2b5bb27fbb3761adce0c5b59d73789e478
Victor <victor246257@gmail.com>
2024-01-25 15:23:35 +0100

    modifica

---
+++ Linkedin Clone/src/components/LearningComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function LearningComponent() {
+  return (
+    <>
+        <div className='d-flex'>
+            <p className='p-2'>
+                
+           </p>
+           <p>
+                ciao
+           </p>
+        </div>
+    </>
+  )
+}
