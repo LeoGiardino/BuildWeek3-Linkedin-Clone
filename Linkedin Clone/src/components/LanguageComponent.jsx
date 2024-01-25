34c2ee259d1bf273ba74b160eb7381170e06e254
Victor <victor246257@gmail.com>
2024-01-25 16:01:41 +0100

    mod

---
+++ Linkedin Clone/src/components/LanguageComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+export default function LanguageComponent() {
+  return (
+    <div className='card'>
+        <div className='card-header'>
+            <h2>Lingua del profilo</h2>
+            <i class="bi bi-pencil"></i>
+        </div>
+    </div>
+  )
+}
