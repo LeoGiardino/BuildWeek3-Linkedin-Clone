d7ab6562e5d9c8d59a7734c56e8c30daecf604c7
Manuel Graziani <145231195+ManuelGraziani@users.noreply.github.com>
2024-01-26 11:33:47 +0100

    Dropdown

--- Linkedin Clone/src/components/Navigazione.jsx
+++ Linkedin Clone/src/components/Navigazione.jsx
@@ -7,6 +7,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/Navbar.css";
 import Form from "react-bootstrap/Form";
 import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/esm/Button";
 
 function Navigazione() {
   return (
@@ -179,13 +180,41 @@ function Navigazione() {
                   Tu
                 </Dropdown.Toggle>
 
-                <Dropdown.Menu style={{ backgroundColor: "#16191C" }}>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Menu style={{ backgroundColor: "#16191C", maxWidth: "300px", border: "1px solid #373A3D", borderRadius: "15px" }} className="dropdown-menu-end">
+                <Dropdown.Item href="#/action-2" className="profileNav" style={{ borderBottom: "1px solid #373A3D" }}>
+                  <div className="d-flex">
+                   <div className='immaginePNav'></div>
+                   <div className="d-flex flex-column ms-2">
+                    <span style={{ color: "#EDEDE3" }}>Leo Giardino</span>
+                    <span style={{ fontSize: "14px", color: "#EDEDE3", fontWeight: "400"}}>Full Stack Developer</span>
+                   </div>
+                  </div>
+                  <div className="d-flex flex-column mt-2" >
+                    <Button className='btn2 fw-bold'>Aggiungi Sezione</Button>
+                   </div>
+                  </Dropdown.Item>
+                  <Dropdown.ItemText style={{ color: "#EDEDE3" }}>Account</Dropdown.ItemText>
                   <Dropdown.Item href="#/action-2">
-                    Another action
+                  <i class="bi bi-slash-square-fill" style={{ color: "orange"}}></i> <span className="premiumHover">Prova Premium per 0 EUR</span>
                   </Dropdown.Item>
                   <Dropdown.Item href="#/action-3">
-                    Something else
+                    Impostazione e privacy
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Guida
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3" style={{ borderBottom: "1px solid #373A3D" }}>
+                    Lingua
+                  </Dropdown.Item>
+                  <Dropdown.ItemText style={{ color: "#EDEDE3" }}>Gestisci</Dropdown.ItemText>
+                  <Dropdown.Item href="#/action-2">
+                    Post e attività
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3" style={{ borderBottom: "1px solid #373A3D" }}>
+                    Account per la pubblicazione di offerte
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Esci
                   </Dropdown.Item>
                 </Dropdown.Menu>
               </div>
@@ -213,13 +242,25 @@ function Navigazione() {
                   Per le aziende
                 </Dropdown.Toggle>
 
-                <Dropdown.Menu style={{ backgroundColor: "#16191C" }}>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">
-                    Another action
+                <Dropdown.Menu style={{ backgroundColor: "#16191C", maxWidth: "300px", border: "1px solid #373A3D", borderRadius: "15px" }} className="dropdown-menu-end">
+                  <Dropdown.ItemText style={{ color: "#EDEDE3" }}>Per le aziende</Dropdown.ItemText>
+                  <Dropdown.Item href="#/action-3">
+                    Assumi su LinkedIn
                   </Dropdown.Item>
                   <Dropdown.Item href="#/action-3">
-                    Something else
+                    Vendi con LinkedIn
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3" >
+                    Offerta di lavoro gratuita
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Fai pubblicità su LinkedIn
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3" >
+                    Impara con LinkedIn
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Centro amministrazione
                   </Dropdown.Item>
                 </Dropdown.Menu>
               </div>
