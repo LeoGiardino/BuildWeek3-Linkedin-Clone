a78fbcd9d90ad30d240d2c8b5406f6b7dcc1a1fc
Kleo <kleoruda9@gmail.com>
2024-01-29 14:14:52 +0100

    Bugfix

--- Linkedin Clone/src/components/homeComponents/CorpoPostsComp.jsx
+++ Linkedin Clone/src/components/homeComponents/CorpoPostsComp.jsx
@@ -21,10 +21,10 @@ export default function CorpoPostsComp() {
         <img style={{ width: '100%' }} src="https://media.licdn.com/dms/image/C4D0BAQGZyFrbDqG6mA/company-logo_100_100/0/1630472646377/bitsoftwaresolution_logo?e=1714608000&v=beta&t=VvajNjrpFPhkEEF1hFCHk8Vzt5bVyYQkAeDq_WlGAcQ" alt="" />
       </div>
       <div style={{ borderTop: '1px solid gray' }} className='d-flex justify-content-between p-3 mt-2'>
-        <div className='d-flex align-items-center'><i class="bi bi-hand-thumbs-up iconPost"></i><span className='fw-bold ms-2'>Consiglia</span></div>
-        <div className='d-flex align-items-center'><i class="bi bi-chat-text iconPost"></i><span className='fw-bold ms-2'>Commenta</span></div>
-        <div className='d-flex align-items-center'><i class="bi bi-arrow-repeat iconPost"></i><span className='fw-bold ms-2'>Diffondi il post</span></div>
-        <div className='d-flex align-items-center'><i class="bi bi-send-fill iconPost"></i><span className='fw-bold ms-2'>Invia</span></div>
+        <div className='d-flex align-items-center'><i className="bi bi-hand-thumbs-up iconPost"></i><span className='fw-bold ms-2'>Consiglia</span></div>
+        <div className='d-flex align-items-center'><i className="bi bi-chat-text iconPost"></i><span className='fw-bold ms-2'>Commenta</span></div>
+        <div className='d-flex align-items-center'><i className="bi bi-arrow-repeat iconPost"></i><span className='fw-bold ms-2'>Diffondi il post</span></div>
+        <div className='d-flex align-items-center'><i className="bi bi-send-fill iconPost"></i><span className='fw-bold ms-2'>Invia</span></div>
       </div>
     </div>
   )
