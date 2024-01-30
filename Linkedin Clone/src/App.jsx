1bc1e4a0ece8b673b768d47f59aff6aa88c815f5
VdanieleM <vdanielem@gmail.com>
2024-01-30 15:34:11 +0100

    Aggiornamenti

--- Linkedin Clone/src/App.jsx
+++ Linkedin Clone/src/App.jsx
@@ -1,31 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import BodyProfile from './pages/BodyProfile'
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BodyProfile from "./pages/BodyProfile";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
-import Navigazione from './components/Navigazione'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import RandomProfile from './pages/RandomProfile';
-import BodyHome from './pages/HomeBody';
-import BodyJobs from './pages/BodyJobs'
-import Footer from './components/Footer';
-import JobsPreferiti from './pages/JobsPreferiti';
+import Navigazione from "./components/Navigazione";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RandomProfile from "./pages/RandomProfile";
+import BodyHome from "./pages/HomeBody";
+import BodyJobs from "./pages/BodyJobs";
+import Footer from "./components/Footer";
+import JobsPreferiti from "./pages/JobsPreferiti";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Navigazione />
-      
-      <Routes>
-        <Route path='/' element={<BodyHome />}/>
-        <Route path='/jobs' element={<BodyJobs />}/>
-        <Route path='/me' element={<BodyProfile />}/>
-        <Route path='/:id' element={<RandomProfile/>}/>
-        <Route path='/jobs/preferiti' element={<JobsPreferiti />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navigazione />
 
+        <Routes>
+          <Route path="/" element={<BodyHome />} />
+          <Route path="/jobs" element={<BodyJobs />} />
+          <Route path="/me" element={<BodyProfile />} />
+          <Route path="/:id" element={<RandomProfile />} />
+          <Route path="/jobs/preferiti" element={<JobsPreferiti />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- Linkedin Clone/src/components/jobsComponents/LavoroCard.jsx
+++ Linkedin Clone/src/components/jobsComponents/LavoroCard.jsx
@@ -1,40 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { XLg, Dot, Linkedin } from 'react-bootstrap-icons';
-import { useDispatch, useSelector } from 'react-redux';
-import { SalvaPreferito } from '../../redux/actions/SalvaPreferito';
+import React, { useEffect, useState } from "react";
+import { XLg, Dot, Linkedin } from "react-bootstrap-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { SalvaPreferito } from "../../redux/actions/SalvaPreferito";
 
-export default function LavoroCard({company, location, titolo, published}) {
+export default function LavoroCard({ company, location, titolo, published }) {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  function handlePrefe (ele) {
-    if(ele.target.className === "bi bi-heart fs-5"){
+  function handlePrefe(ele) {
+    if (ele.target.className === "bi bi-heart fs-5") {
       dispatch(SalvaPreferito(oggPref)),
-      ele.target.className = "bi bi-heart-fill fs-5"
-    }else{
-      ele.target.className = "bi bi-heart fs-5"
+        (ele.target.className = "bi bi-heart-fill fs-5");
+    } else {
+      ele.target.className = "bi bi-heart fs-5";
     }
   }
 
-  const [oggPref, setOggPref] = useState({})
+  const [oggPref, setOggPref] = useState({});
   // console.log(oggPref)
-  useEffect(()=>{
-    setOggPref({titolo, company, location, published})
-  },[])
+  useEffect(() => {
+    setOggPref({ titolo, company, location, published });
+  }, []);
   return (
     <>
-        <div className='d-flex justify-content-between flex-column align-items-start mb-4 expRow'>
-                    <div className='d-flex justify-content-between align-items-start consigliatiGroup w-100'>
-                        <div className='imgAziendaCons me-3'><img src='https://media.licdn.com/dms/image/C560BAQFDryFqR1ruog/company-logo_100_100/0/1630577965936/plantronic_logo?e=1714608000&v=beta&t=6_nBqF0wyvP6NTv4BvtvWDk6xp19vzI3mIMRdfKWwq8'></img></div>
-                        <div className='d-flex flex-column w-100'>
-                            <div className='d-flex justify-content-between align-items-center'><span className='ente consigliatiPosizione'>{titolo}</span><XLg className='iconcineCons' /></div>
-                            <span className='nomeFormazione'>{company}</span>
-                            <span className='periodoFormazione'>{location}</span>
-                            <p className='periodoFormazione secondoSottotitolo'>{new Date(published).toLocaleDateString()}<span className='float-end'><i onClick={(ele) => 
-                              handlePrefe(ele)} className="bi bi-heart fs-5"></i></span></p>   
-                        </div>
-                    </div>
+      <div className="d-flex justify-content-between flex-column align-items-start mb-4 expRow">
+        <div className="d-flex justify-content-between align-items-start consigliatiGroup w-100">
+          <div className="imgAziendaCons me-3">
+            <img src="https://media.licdn.com/dms/image/C560BAQFDryFqR1ruog/company-logo_100_100/0/1630577965936/plantronic_logo?e=1714608000&v=beta&t=6_nBqF0wyvP6NTv4BvtvWDk6xp19vzI3mIMRdfKWwq8"></img>
+          </div>
+          <div className="d-flex flex-column w-100">
+            <div className="d-flex justify-content-between align-items-center">
+              <span className="ente consigliatiPosizione">{titolo}</span>
+            </div>
+            <span className="nomeFormazione">{company}</span>
+            <span className="periodoFormazione">{location}</span>
+            <p className="periodoFormazione secondoSottotitolo">
+              {new Date(published).toLocaleDateString()}
+              <span className="float-end">
+                <i
+                  onClick={(ele) => handlePrefe(ele)}
+                  className="bi bi-heart fs-5"
+                ></i>
+              </span>
+            </p>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
